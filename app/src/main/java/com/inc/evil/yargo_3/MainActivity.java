7758d9b20996d00d23c21b29a394fc8c57af0822
JoHnRembo <happinessss57@gmail.com>
2019-03-17 20:12:05 +0300

    first coomit

--- app/src/main/java/com/inc/evil/yargo_3/MainActivity.java
+++ app/src/main/java/com/inc/evil/yargo_3/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String t ="dfsfd";
+        String t ="dfsfd123123";
     }
 }
