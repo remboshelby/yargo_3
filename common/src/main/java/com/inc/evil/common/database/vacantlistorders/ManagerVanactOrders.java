5143025ea0f96164b918b81a095d29a492dcd6a5
JoHnRembo <happinessss57@gmail.com>
2019-03-24 21:36:24 +0300

    dagger first

--- common/src/main/java/com/inc/evil/common/database/vacantlistorders/ManagerVanactOrders.java
+++ common/src/main/java/com/inc/evil/common/database/vacantlistorders/ManagerVanactOrders.java
@@ -1,4 +1,14 @@
 package com.inc.evil.common.database.vacantlistorders;
 
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+
 public interface ManagerVanactOrders {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(VacantOrdersList vacantOrdersList);
+
+    @Delete
+    void remove(VacantOrdersList vacantOrdersList);
+
 }
